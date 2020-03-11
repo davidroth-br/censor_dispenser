69723d02c005273a4583b3ea55ec251076aefec7
David <61717416+davidroth-br@users.noreply.github.com>
2020-03-11 12:34:41 -0400

    Same thing, modified logic

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -1,4 +1,5 @@
-# These are the emails you will be censoring. The open() function is opening the text file that the emails are contained in and the .read() method is allowing us to save their contexts to the following variables:
+# These are the emails you will be censoring. The open() function is opening the text file that the emails are
+# contained in and the .read() method is allowing us to save their contexts to the following variables:
 email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
@@ -6,9 +7,10 @@ email_four = open("email_four.txt", "r").read()
 
 proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithms", "her", "herself"]
 negative_words = ["concerned", "behind", "danger", "dangerous", "danger", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
-punctuation = ["", ".", ",", "?", "!", ":", ";", "'s"]
+punctuation = [".", ",", "?", "!", ":", ";"]
 
 censored = "*"
+
 
 def prepare_text(text):
     broken_down_text = []
@@ -17,22 +19,40 @@ def prepare_text(text):
         broken_down_text.append(line.split())
     return broken_down_text
 
+
 def reformat_text(prepared_text):
     lines = []
     for line in prepared_text:
         lines.append(" ".join(line))
     return "\n".join(lines)
 
-def compare_words(word_to_censor, word_in_text):
+
+def prepare_punctuation(censor, word, length):
+    censored_word = ""
     for punct in punctuation:
-        if word_to_censor in word_in_text:
-            test_word = word_to_censor + punct
-            if test_word == word_in_text.lower():
-                return True, punct
-    return False, punct
+        if punct == word[-1:]:
+            word_without_punct = word[:-1]
+            if censor == word_without_punct:
+                censored_word = (censored * (length - 1)) + punct
+                break
+        else:
+            if censor == word:
+                censored_word = censored * length
+    return censored_word
+
+
+def compare_words(censor, word, lengths):
+    censored_words = []
+    if censor in word.lower():
+        for length in lengths:
+            censored_word = prepare_punctuation(censor, word.lower(), length)
+            if censored_word != "":
+                censored_words.append(censored_word)
+    return censored_words
+
 
 def censor_negatives(negatives, proprietaries, text):
-    pre_censored_text = censor_many_words(proprietaries, text)
+    pre_censored_text = censor(proprietaries, text, "simple")
     new_text = pre_censored_text
     for word in negatives:
         if pre_censored_text.count(word) >= 2:
@@ -40,51 +60,50 @@ def censor_negatives(negatives, proprietaries, text):
             pre_censored_text = new_text
     return new_text
 
+
 def censor(list_of_words_to_censor, text, method):
     text_list = prepare_text(text)
     for line_number in range(len(text_list)):
         for word_number in range(len(text_list[line_number])):
             for word_to_censor in list_of_words_to_censor:
-                if " " in word_to_censor and (word_number + 1) < len(text_list[line_number]):
-                    need_to_censor, punct = compare_words(word_to_censor, text_list[line_number][word_number] + " " + text_list[line_number][word_number + 1])
-                    two_words = True
-                else:
-                    need_to_censor, punct = compare_words(word_to_censor, text_list[line_number][word_number])
-                    two_words = False
-                if need_to_censor:
-                    if method == "full":
-                        text_list[line_number][word_number - 1] = censored * len(text_list[line_number][word_number - 1])
-                        if punct == "'s" or punct == "'d":
-                            text_list[line_number][word_number] = censored * len(text_list[line_number][word_number])
-                        else:
-                            text_list[line_number][word_number] = censored * (len(text_list[line_number][word_number]) - len(punct)) + punct
-                        try:
-                            text_list[line_number][word_number + 1] = censored * len(text_list[line_number][word_number + 1])
-                        except:
-                            pass
-                    elif method == "simple":
-                        if punct == "'s" or punct == "'d":
-                            text_list[line_number][word_number] = censored * len(text_list[line_number][word_number])
-                            if two_words:
-                                text_list[line_number][word_number + 1] = censored * len(
-                                    text_list[line_number][word_number + 1])
-                        else:
-                            if two_words:
-                                text_list[line_number][word_number] = censored * len(text_list[line_number][word_number])
-                                text_list[line_number][word_number + 1] = censored * (len(
-                                    text_list[line_number][word_number + 1]) - len(punct)) + punct
-                            else:
-                                text_list[line_number][word_number] = censored * (len(text_list[line_number][word_number]) - len(punct)) + punct
+                number_of_words = len(word_to_censor.split())
+
+                word_to_check = ""
+                word_lengths = []
+                for i in range(number_of_words):
+                    try:
+                        word_to_check += text_list[line_number][word_number + i] + " "
+                        word_lengths.append(len(text_list[line_number][word_number + i]))
+                    except:
+                        break
+                formated_word = word_to_check.strip()
+
+                censored_words = compare_words(word_to_censor, formated_word, word_lengths)
+                if censored_words:
+                    if method == "simple" or method == "full":
+                        for i in range(len(censored_words)):
+                            text_list[line_number][word_number + i] = censored_words[i]
+                            last_index = i
+                        if method == "full":
+                            word_before = word_number - 1
+                            word_after = word_number + last_index + 1
+                            text_list[line_number][word_before] = prepare_punctuation(text_list[line_number][word_before], text_list[line_number][word_before], len(text_list[line_number][word_before]))
+                            try:
+                                text_list[line_number][word_after] = prepare_punctuation(text_list[line_number][word_after], text_list[line_number][word_after], len(text_list[line_number][word_after]))
+                            except:
+                                pass
+
     return reformat_text(text_list)
 
+
 #print(email_one)
-#print(censor(["learning algorithms"], email_one, "simple"))
+print(censor(["learning algorithms"], email_one, "simple"))
 
 #print(email_two)
-#print(censor(proprietary_terms, email_two, "simple"))
+print(censor(proprietary_terms, email_two, "simple"))
 
 #print(email_three)
 #print(censor_negatives(negative_words, proprietary_terms, email_three))
 
 #print(email_four)
-#print(censor(negative_words + proprietary_terms, email_four, "full"))
+print(censor(negative_words + proprietary_terms, email_four, "full"))
